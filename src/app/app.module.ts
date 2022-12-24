170ee8e1377999a46e96d1c1c294239cb5810a56
Abdullah Abdullah <abdullah141299@gmail.com>
2022-12-24 02:07:27 +0000

    initialised tailwindcss. added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { CollectionComponent } from './collection/collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
