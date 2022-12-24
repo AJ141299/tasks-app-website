4ac7341f7f1d6cfe2c54ac6742e366c06a4d1e4a
Abdullah Abdullah <abdullah141299@gmail.com>
2022-12-24 03:42:00 +0000

    shifted everything to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskComponent } from './task/task.component';
-import { CollectionComponent } from './collection/collection.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CollectionsComponent } from './collections/collections.component';
+import { TaskComponent } from './components/task/task.component';
+import { CollectionComponent } from './components/collection/collection.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CollectionsComponent } from './components/collections/collections.component';
 
 @NgModule({
   declarations: [
