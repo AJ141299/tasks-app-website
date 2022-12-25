1fe218ee8a40da403a441385772f9ac536172600
Abdullah Abdullah <abdullah141299@gmail.com>
2022-12-25 01:13:10 +0000

    started tasks page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task/task.component';
-import { CollectionComponent } from './components/collection/collection.component';
+import { TaskComponent } from './components/tasks/task/task.component';
+import { CollectionComponent } from './components/collections/collection/collection.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CollectionsComponent } from './components/collections/collections.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './state/app.state';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { AddCollectionModalComponent } from './components/add-collection-modal/add-collection-modal.component';
+import { AddCollectionModalComponent } from './components/collections/add-collection-modal/add-collection-modal.component';
 import { BlockScreenComponent } from './components/block-screen/block-screen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     NavbarComponent,
     CollectionsComponent,
     AddCollectionModalComponent,
-    BlockScreenComponent
+    BlockScreenComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
